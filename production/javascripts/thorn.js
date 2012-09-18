840093800107c3eb56133a0993233e8261987f49
Mike Newell <mike@iwearshorts.com>
2012-09-17 20:03:30 -0700

    added transitions for graph and some minor font size tweaks for follow us text

--- production/javascripts/thorn.js
+++ production/javascripts/thorn.js
@@ -1,17 +1,17 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 (function($) {
     $(window).load(function() {
+/* -----------------------------------------------------------------------------
+ *                              Page Attributes
+ * ---------------------------------------------------------------------------*/
 
-        // page attributes
         var windowWidth = 0;
         var taskForceHoverEnabled = false;
+        var graphActive, graphActiveSelector;
 
-        // set some events
+/* -----------------------------------------------------------------------------
+ *                              Events
+ * ---------------------------------------------------------------------------*/
+
         $('#taskForceContainer .panelBlack').on('mouseover touchstart', function() {
             if(taskForceHoverEnabled) {
                 updateTaskForceHeight(true);
@@ -28,71 +28,30 @@
             checkWidthForMobileNavbar();
         });
         
-        $('#graphContainer').on('mousemove', function(evt) {
+        $('#graphSelectorContainer li a').on('click mouseover', function(evt) {
             
-//            var container = $('#graphContainer');
-//            var containerOffset = $('#graphContainer').offset();
-//            
-//            console.dir(container);
-//            
-//            var offsetX = evt.pageX - containerOffset.left;
-//            var offsetY = evt.pageY - containerOffset.top;
-//            
-//            var originalStyle = (1024 * offsetX / container[0].offsetWidth) + '%' + (250 * offsetY / container[0].offsetHeight) + '%';
-//            
-//            container[0].style.webkitPerspectiveOrigin = originalStyle;
-
-
-//            var container = $('#graphContainer'),
-//                containerOffset = container.offset();
-//            
-//            var offsetX = evt.pageX - containerOffset.left;
-//            var offsetY = evt.pageY - containerOffset.top;
-//            
-//            console.log('x:' + offsetX + ' y: ' + offsetY);
-//            
-//            container.css({
-//                '-webkit-perspective-origin' : '50% 50% 50%',
-//                '-webkit-tranform-style' : 'preserve-3d',
-//                '-webkit-transform' : 'perspective(0) translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotate3d(0, 1, 0, '+-(offsetX/8)+'deg)'
-//            });
-
-            var img1 = $('.graphBG'),
-                img2 = $('.graphRed'),
-                img3 = $('.graphLightGrey'),
-                img4 = $('.graphDarkGrey'),
-                container = $('#graphContainer');
-                
-            var left = evt.clientX - (container[0].offsetWidth/2),
-                top = evt.clientY - (container[0].offsetHeight/2);
-                
-            container.css({
-                '-webkit-transform' : 'rotateY(' + Math.floor(Math.abs((evt.clientX - (container[0].offsetWidth / 2))/15)) + 'deg)'
-            });
- 
-            img1.css({
-                'left': -1 * ((left)/4) + 'px',
-                'top' : -1 * ((top)/4) + 'px'
-            });
+            evt.preventDefault();
             
-            img2.css({
-                'left': -1 * ((left)/3.5) + 'px',
-                'top' : -1 * ((top)/3.5) + 'px'
-            });
+            if(typeof graphActive != "undefined") {
+                graphActive.removeClass('graphActive');
+            }
             
-            img3.css({
-                'left': -1 * ((left)/3) + 'px',
-                'top' : -1 * ((top)/3) + 'px'
-            });
+            if(typeof graphActiveSelector != "undefined") {
+                graphActiveSelector.removeClass('graphActiveSelector');
+            }
             
-            img4.css({
-                'left': -1 * ((left)/2.5) + 'px',
-                'top' : -1 * ((top)/2.5) + 'px'
-            });
+            $(this).addClass('graphActiveSelector');
+            
+            var id = $(this).attr('href');
+            graphActive = $(id);
+            graphActive.addClass('graphActive');
             
         });
 
-        // helpers
+/* -----------------------------------------------------------------------------
+ *                              Helpers
+ * ---------------------------------------------------------------------------*/
+
         var init = function() {
             checkWidthForMobileNavbar();
         };
@@ -133,8 +92,11 @@
         var updateWindowWidth = function() {
             windowWidth = $(window).width();
         };
+        
+/* -----------------------------------------------------------------------------
+ *                              Initialize
+ * ---------------------------------------------------------------------------*/
 
-        // do some stuff
         init();
 
     });
