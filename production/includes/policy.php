fbd6075635c632f49d4f4e196021bc4a14465396
Mike Newell <mike@iwearshorts.com>
2012-11-05 17:16:18 -0800

    added andreas changes to stylesheet

--- production/includes/policy.php
+++ production/includes/policy.php
@@ -5,8 +5,8 @@
 ?>
             <div id="privacyPolicy" class="row">
                 <div class="twelve columns">
-                    <hr class="goldhr" />
-                    <a href="#">Privacy Policy</a>
+                    <hr class="privacyhr" />
+                    <a href="#">PRIVACY POLICY +</a>
                     <div id="privacyContainer">
                         <p>THORN is committed to providing information and activities relating to THORN'S mission on the website. While we encourage the interactive nature of our website, we strive to promote the online safety and privacy of our users.</p>
                         <p>Information You Provide to Us: We collect information about you when you visit and interact with this site. Some of this information is automatically collected, and some is collected when you interact with the site. The type of information that is automatically collected can include date and time of access of the site, the Internet protocol address of the computer that you are using, the domain and host from which you access the Internet, your browser software and operating system, and the specific activities you are engaged in while visiting the site. Information from which you can be personally identified may also be collected by us at the site, including your name and email address.</p>
