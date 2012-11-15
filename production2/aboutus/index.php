110216ec4f947a6e57eeb382139bbce26547d40d
Mike Newell <mike@iwearshorts.com>
2012-11-14 20:10:14 -0800

    adding intro animation

--- production2/aboutus/index.php
+++ production2/aboutus/index.php
@@ -1,44 +1,9 @@
-<?php include_once '../includes/header.php'; ?>
-                <section id="aboutus">
-                    <article id="digitalDefenders" class="panelDarkgrey">
-                        <hr class="digitalhr" />
-                        <h2>WE ARE DIGITAL DEFENDERS OF CHILDREN</h2>
-                    </article>
-
-                    <article id="blurb" class="panelRed">
-                        <hr class="whitehr" />
-                        <p>Thorn exists to continue the work started by the Demi and Ashton (DNA) Foundation in 2009.</p>
-                        <p>We aim to disrupt and deflate the predatory behavior of those who abuse and traffic children, solicit sex with children or create and share child pornography.</p>
-                        <p>As these crimes are increasingly facilitated by technology, we invest in and deploy the latest technology as part of our ongoing fight to end child sexual exploitation.</p>
-                    </article>
-
-                    <article id="board" class="panelLightgrey">
-                        <hr class="blackhr" />
-                        <a href="#boardExpanded">BOARD OF DIRECTORS <span>+</span></a>
-                        <div id="boardExpanded">
-                            <h3>ASHTON KUTCHER</h3>
-                            <p>Cofounder of Thorn. Kutcher is an actor, producer, tech and social media investor, and philanthropist focused on using technology to effect positive changes in the world. In 2010 Time Magazine named him among their Top 100 Most Influential People; the following year, Vanity Fair honored him in their 2011 New Establishment List, which identifies the top 50 of an innovative new breed of buccaneering visionaries, engineering prodigies and entrepreneurs.</p>
-                            <h3>DEMI MOORE</h3>
-                            <p>Cofounder of Thorn. Moore is an actress and a long-standing advocate for women’s issues. She received a Golden Globe nomination for her role in If These Walls Could Talk and a Director’s Guild nomination for Five, an anthology of short films that explore the impact of breast cancer. She partnered with CNN Freedom Project for the documentary Nepal’s Stolen Children to highlight the work of antitrafficking advocate Anuradha Koirala. In 2011 she became executive producer of “The Conversation,” an interview series hosted by Amanda de Cadenet that features intimate and frank interviews with prominent women.</p>
-                            <h3>RAY CHAMBERS</h3>
-                            <p>United Nations Secretary-General’s Millennium Development Goals (MDG) Advocate and Special Envoy for Malaria. Chambers is a philanthropist and humanitarian who directs most of his efforts toward helping children. He is the founding chairman of the Points of Light Foundation and cofounder, with Colin Powell, of America’s Promise—The Alliance for Youth. He also cofounded the National Mentoring Partnership, the Millennium Promise Alliance and Malaria No More.</p>
-                            <h3>JIM PITKOW</h3>
-                            <p>Chair of the Thorn Technology Task Force. Pitkow is a Silicon Valley entrepreneur, investor, and advisor. He received a Doctorate in Computer Science from the Georgia Institute of Technology. Jim is a member of the National Ski Patrol and an avid student of the contemplative arts.</p>
-                        </div>
-                    </article>
-
-                    <article id="taskForce" class="panelBlack">
-                        <hr class="goldhr" />
-                        <a href="#techTaskForceExpanded">TECH TASK FORCE MEMBERS <span>+</span></a>
-                        <div id="taskForceExpanded">
-                            <img src="../images/aboutus/taskForce.png" />
-                        </div>
-                    </article>
-
-                </section>
-                
-                <?php include_once '../includes/footer.php'; ?>
-            </div>
-            <?php include_once '../includes/sidebar.php'; ?>
-                
-<?php include_once '../includes/end.php'; ?>
+<?php
+    if (preg_match('~MSIE|Internet Explorer~i', $_SERVER['HTTP_USER_AGENT'])) {
+//        echo 'IE detected';
+        include_once 'main.php';
+    } else {
+//        echo 'IE not detected';
+        include_once 'index_content.php';
+    }
+?>
